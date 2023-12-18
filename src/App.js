7c66ae8785ae07134be668b829d5f661ec3c1523
Oleksandr Kovshar <aleksandr.mpro@gmail.com>
2023-12-18 23:25:36 +0200

    add Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Counter from "./components/Counter/Counter";
+// import Dropdown from "./components/Dropdown/Dropdown";
+// import ColorPicker from "./components/ColorPicker/ColorPicker";
+// import TodoList from "./components/TodoList/TodoList";
+
+const ColorPicker = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Состояние компонента</h1>
+      <Counter initialValue={100} />
+
+      {/* <Dropdown /> */}
+      {/* <ColorPicker /> */}
+      {/* <TodoList /> */}
     </div>
   );
 }
