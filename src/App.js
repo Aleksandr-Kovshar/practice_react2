34cd363c03fdc6b75d1ebd1f7bbad47f71fbb073
Oleksandr Kovshar <aleksandr.mpro@gmail.com>
2024-05-08 21:51:50 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
-// import Counter from "./components/Counter";
-// import Dropdown from "./components/Dropdown";
-// import ColorPicker from "./components/ColorPicker";
+import Counter from "./components/Counter";
+import Dropdown from "./components/Dropdown";
+import ColorPicker from "./components/ColorPicker";
 import TodoList from "./components/TodoList";
 import initialTodos from "./todos.json";
 
-// const ColorPickerOptons = [
-//   { label: "red", color: "#F44336" },
-//   { label: "green", color: "#4CAF50" },
-//   { label: "blue", color: "#2196F3" },
-//   { label: "grey", color: "#607D8B" },
-//   { label: "pink", color: "#E91E63" },
-//   { label: "indigo", color: "#3F51B5" },
-// ];
+const ColorPickerOptons = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
 
 // function App() {
 //   return (
@@ -38,7 +38,7 @@ class App extends Component {
     this.setState((prevState) => ({
       todos: prevState.todos.filter((todo) => todo.id !== todoId),
     }));
-  };
+  }; 
 
   render() {
     const { todos } = this.state;
@@ -51,9 +51,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Состояние компонента</h1>
-        {/* <Counter initialValue={0} /> */}
-        {/* <ColorPicker options={ColorPickerOptons} /> */}
-        {/* <Dropdown /> */}
+        <Counter initialValue={0} />
+        <ColorPicker options={ColorPickerOptons} />
+        <Dropdown />
 
         <div>
           <p>Общее количество задач: {totalTodoCount}</p>
